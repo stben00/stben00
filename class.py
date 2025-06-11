efc02f70f7451b928ec402331bbc0ca3d07e20bd
stben00 <bekzatruslanuulu@gmail.com>
2025-06-11 20:40:37 +0000

    class

---
+++ class.py
@@ -0,0 +1,18 @@
+class House():
+    ### Описание Дома"""
+    
+    def __init__(self,street,number):
+        self.street = street
+        self.number = number
+    ### Свойства Дома"""                           
+
+    def build(self):    
+    ### Сторить Дом " 
+
+     print("Дом на улеце" + self.street, "под номером" + str(self.number) + "построен")
+          
+House1 = House("Овражная", 31)
+House2 = House("Логвиненко", 7)
+
+
+print(House2.street)
